865ba3e76c5ff7132b4690f57f10d43facbae5e7
dylanfox44 <131827295+dylanfox44@users.noreply.github.com>
2025-02-10 19:27:36 +0000

    update

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,3 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
-
-ScrollReveal().reveal('.story', {
-  delay: 500,
-  interval: 200
-});
